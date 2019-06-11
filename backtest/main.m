2ca188e63853e3af8ee3f593559f68626a0f6c5c
Capricorn2018 <zhouxinyu882003@163.com>
2019-06-11 15:59:17 +0800

    main.m updated

--- backtest/main.m
+++ backtest/main.m
@@ -1,5 +1,12 @@
-clear all;
-clc;
+assets_file = 'D:/Projects/macro_test/国开债指数.xlsx';
+[nav,ret,reb] = load_assets(assets_file,5,'last');
 
-filename = 'D:/Projects/macro_test/国开债指数.xlsx';
-[nav,ret,reb] = load_assets(filename,5,'last');
+factor_file = 'D:/Projects/macro_test/国债期限利差.xls';
+factor = load_factor(factor_file,reb);
+
+tbl = merge_xy(factor, ret);
+
+
+
+
+
