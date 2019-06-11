c4e099142b71236a6f9240edd2aaa9e0b35db272
Xinyu Zhou <zhouxinyu882003@163.com>
2019-06-11 19:29:42 +0800

    some changes in main.m and load_factors.m, factor mean columns in factor tables are revised

--- backtest/load_assets.m
+++ backtest/load_assets.m
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-function [nav,ret] = load_assets(filename,num_days,direction)
-% 从Excel中读入指数历史水平，取每个月的某个交易日，并计算区间收益
-
-=======
 function [nav,ret,reb] = load_assets(filename,num_days,direction)
 %LOAD_ASSETS 此处显示有关此函数的摘要
 %   此处显示详细说明
->>>>>>> 2ca188e63853e3af8ee3f593559f68626a0f6c5c
+
      raw = readtable(filename);
      
     raw.Properties.VariableNames(1) = {'date'};
@@ -22,13 +17,8 @@ function [nav,ret,reb] = load_assets(filename,num_days,direction)
 
     for i = 1:length(reb)
 
-<<<<<<< HEAD
-        if(i>1)
-            ret(i,2:end) = array2table(table2array(raw(Locb(i),2:end))./table2array(raw(Locb(i-1),2:end)) - 1);
-=======
         if(i<length(reb))
             ret(i,2:end) = array2table(table2array(raw(Locb(i+1),2:end))./table2array(raw(Locb(i)+1,2:end)) - 1);
->>>>>>> 2ca188e63853e3af8ee3f593559f68626a0f6c5c
         else
             ret(i,2:end)= array2table(nan(1,size(ret,2)-1));
         end
--- backtest/load_factor.m
+++ backtest/load_factor.m
@@ -1,27 +1,23 @@
-<<<<<<< HEAD
-function result = load_factor(filename,num_days,direction)
-% 从Excel中读入某个因子数据，取某个月的固定交易日，并计算区间均值
-=======
-function result = load_factor(filename,reb)
+
+function result = load_factor(filename,reb,factor_name)
 % 此处显示有关此函数的摘要
 %   reb: rebalance_dates, 这里就是用load_assets里面的date就行
->>>>>>> 2ca188e63853e3af8ee3f593559f68626a0f6c5c
     
     raw = readtable(filename);
-    raw.Properties.VariableNames = {'date','factor'};
+    raw.Properties.VariableNames = {'date',factor_name};
     raw.date = datenum(raw.date);
         
     [~,Locb] = ismember(reb,raw.date);
     
     result = raw(Locb,:);
-    result.mean = zeros(height(result),1);
+    eval(['result.mean_',factor_name,' = zeros(height(result),1);'];
     
     for i = 1:length(reb)
         
         if(i>1)
-            result.mean(i) = mean(raw.factor((Locb(i-1)+1):Locb(i)));
+            eval(['result.mean_',factor_name,'(i)= mean(raw.',factor_name,'((Locb(i-1)+1):Locb(i)));']);
         else
-            result.mean(i) = mean(raw.factor(1:Locb(i)));
+            eval(['result.mean_',factor_name,'(i) = mean(raw.',factor_name,'(1:Locb(i)));']);
         end
     end    
 
