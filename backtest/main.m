7a03f397be54bf61d67e19d94d76c3eaa8e21c40
Capricorn2018 <zhouxinyu882003@163.com>
2019-06-14 16:22:16 +0800

    minor changes

--- backtest/main.m
+++ backtest/main.m
@@ -54,22 +54,38 @@ signal_sprd = roll_signal(sprd51,36,0.75);
 signal_mom = mom1>0;
 [r_mom,alpha_mom] = long_short(r_long,r_short,signal_mom);
 
+signal_fin = roll_signal(sprdfin,36,0.75);
+[r_fin,alpha_fin] = long_short(r_long,r_short,signal_fin);
+
 % 两个策略各分1/4的权重,也就是最多久期估计也就4不到
-r_all = r_mom/4 + r_sprd/4 + r1/2;
+r_all = r_mom/6 + r_sprd/6 + r_fin/6 + r1/2;
 alpha = r_all - r1;
 
 
 
 nav = [1;cumprod(1+r_all)];
-nav1 = [1;cumprod(1+r1)];
+nav_short = [1;cumprod(1+r_short)];
 nav_long = [1;cumprod(1+r_long)];
+nav_mom = [1;cumprod(1+r_mom)];
+nav_sprd = [1;cumprod(1+r_sprd)];
+nav_fin = [1;cumprod(1+r_fin)];
 
-hold on;
+figure(1);
 plot(nav);
-plot(nav1);
+hold on;
+plot(nav_short);
 plot(nav_long);
 hold off;
 
+figure(2);
+plot(nav_mom,'b');
+hold on;
+plot(nav_sprd,'r');
+plot(nav_fin,'g');
+plot(nav_short,'p');
+hold off;
+
+figure(3);
 yr = 2005:2019;
 [alpha_yr,r_yr] = year_stats(alpha,r_all,tbl.date,yr);
 bar(alpha_yr);
