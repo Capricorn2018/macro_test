e96e2a038734f5e6175e3ced164bd1a628476991
Capricorn2018 <zhouxinyu882003@163.com>
2019-07-31 11:23:10 +0800

    main.m revised in /cycle

--- cycle/main.m
+++ cycle/main.m
@@ -12,11 +12,16 @@ isnum = ~any(isnan(data),2);
 
 data = [econ(isnum,1),array2table(data(isnum,:))];
 
-start = datenum(2015,1,1);
+start = datenum(2009,1,1);
 bl = data.times >= start;
 
 id = 1:length(data.times);
 n_st = min(id(bl));
+
+theta = zeros(length(data.times),1);
+A = zeros(length(data.times),1);
+pd = makedist('Normal',238,10);
+weight = pdf(pd,1:10000);
 
 for i = n_st:length(data.times)
     
@@ -31,6 +36,10 @@ for i = n_st:length(data.times)
     
     % pca因子加权结果
     index = tmp * coeff1;
+    F = fft(index((end-50):end),10000);
+    
+    theta(i) = angle(F(238)); % 提取42个月的相位
+    A(i) = (weight * abs(F))/sum(weight);
     
 end
 
