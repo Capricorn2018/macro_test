d0df342023b465080072c86707ee8a7a8758078b
Capricorn2018 <zhouxinyu882003@163.com>
2019-08-09 17:41:24 +0800

    some minor changes

--- converts/macro.m
+++ converts/macro.m
@@ -1,7 +1,7 @@
 function [ output_args ] = macro( input_args )
 %MACRO 此处显示有关此函数的摘要
 %   此处显示详细说明
-
+     [w_edb_data,w_edb_codes,w_edb_fields,w_edb_times,w_edb_errorid,w_edb_reqid]=w.edb('M0001385','2002-06-09','2019-08-09');
 
 end
 
