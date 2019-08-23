9fdf8163ef0825e0b1baa3859ef43d938339dbb4
Capricorn2018 <zhouxinyu882003@163.com>
2019-08-23 17:51:53 +0800

    some changes in /alpha

--- alpha/load_factor.m
+++ alpha/load_factor.m
@@ -17,15 +17,34 @@ function result = load_factor(filename,reb)
     
     for k = 2:size(raw,2)
         factor_name = raw.Properties.VariableNames{k};
-        eval(['result.mean_',factor_name,' = zeros(height(result),1);']);
+        eval(['result.mean_',factor_name,' = nan(height(result),1);']);
+        
+        eval(['result.vol_',factor_name,'_1 = nan(height(result),1);']);
+        eval(['result.vol_',factor_name,'_3 = nan(height(result),1);']);
+        eval(['result.vol_',factor_name,'_6 = nan(height(result),1);']);
 
         for i = 1:length(Locb)
 
             if(i>1)
                 eval(['result.mean_',factor_name,'(i)= mean(raw.',factor_name,'((Locb(i-1)+1):Locb(i)));']);
+                eval(['result.vol_',factor_name,'_1(i)= std(raw.',factor_name,'((Locb(i-1)+1):Locb(i)));']);
             else
                 eval(['result.mean_',factor_name,'(i) = mean(raw.',factor_name,'(1:Locb(i)));']);
+                eval(['result.vol_',factor_name,'_1(i)= std(raw.',factor_name,'(1:Locb(i)));']);
             end
+            
+            if(i>3)
+                eval(['result.vol_',factor_name,'_3(i)= std(raw.',factor_name,'((Locb(i-3)+1):Locb(i)));']);
+            else
+                eval(['result.vol_',factor_name,'_3(i)= std(raw.',factor_name,'(3:Locb(i)));']);
+            end
+            
+            if(i>6)
+                eval(['result.vol_',factor_name,'_6(i)= std(raw.',factor_name,'((Locb(i-6)+1):Locb(i)));']);
+            else
+                eval(['result.vol_',factor_name,'_6(i)= std(raw.',factor_name,'(6:Locb(i)));']);
+            end
+            
         end    
     end
 
--- stock/macro.m
+++ stock/macro.m
@@ -1,11 +1,11 @@
-function [macro_data,macro_times] = macro(start_dt,end_dt)
+function [macro_data,macro_times,yield_data,yield_times] = macro(start_dt,end_dt)
 % 读取宏观数据
 
     w = windmatlab;
     % CPI,PPI,M2,M1
     [macro_data,~,~,macro_times,~,~]=w.edb('M0000612,M0001227,M0001385,M0001383',start_dt,end_dt);
-    % 国债1,3,5,10,国开10,农发10,收益率
-    [yield_data,yield_codes,~,yield_times,~,~] = w.edb('S0059744,S0059746,S0059747,S0059749,M1004271,M1007675',start_dt,end_dt,'Fill=Previous'); 
+    % 国债1,3,5,10,国开10,农发10,收益率, 这里暂时打算回测一下期限利差过于窄的时候是不是对股票相对收益有影响
+    [yield_data,~,~,yield_times,~,~] = w.edb('S0059744,S0059746,S0059747,S0059749,M1004271,M1007675',start_dt,end_dt,'Fill=Previous'); 
 
 end
 
