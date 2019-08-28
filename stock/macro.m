c9073c3473b0c6a26fc26064dc867410613fda65
Capricorn2018 <zhouxinyu882003@163.com>
2019-08-28 12:05:18 +0800

    some changes in /stock/macro.m/

--- stock/macro.m
+++ stock/macro.m
@@ -10,7 +10,8 @@ function [macro_data,macro_times,yield_data,yield_times] = macro(start_dt,end_dt
                                             'M0068071,M0000559,M0000557,M5541321'],start_dt,end_dt);
     % 国债1,3,5,10,国开10,农发10,收益率, 这里暂时打算回测一下期限利差过于窄的时候是不是对股票相对收益有影响
     [yield_data,~,~,yield_times,~,~] = w.edb(['S0059744,S0059746,S0059747,S0059749,',...
-                                                'M1004271,M1007675'],start_dt,end_dt,'Fill=Previous'); 
+                                                'M1004271,M1007675'],start_dt,end_dt,...
+                                                'Fill=Previous'); 
 
     
 end
