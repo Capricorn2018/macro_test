2a16bfc4d4a31a14c94340fa3f255219de58360b
Capricorn2018 <zhouxinyu882003@163.com>
2020-01-15 09:57:22 +0800

    liq_perm.m revised, /macro_factor folder restructured

--- macro_factor/indicators/liq_prem.m
+++ macro_factor/indicators/liq_prem.m
@@ -3,29 +3,26 @@ function [ output_args ] = liq_prem(start_dt,end_dt)
 %   此处显示详细说明
     w = windmatlab;
     
-    % 利率互换收盘价, FR007 3M, FR007 1Y, FR007 5Y, SHIBOR互换1Y, SHIBOR 3M
-    [irs,irs_codes,~,irs_times,~,~]=w.wsd(['FR007S3M.IR,','FR007S1Y.IR,','FR007S5Y.IR,',...
-                                            'SHI3MS1Y.IR,','SHIBOR3M.IR'],...
-                                             'close',start_dt,end_dt);
-    
-    % 国债1Y, 同业存单AAA 3m, 国开1Y
-    [yield,yield_codes,~,yield_times,~,~] = w.edb('S0059744,M1004899,M1004263',...
-                                                start_dt,end_dt,...
-                                                'Fill=Previous'); 
+    % 国债1Y, 同业存单AAA 3m, 国开1Y, FR007互换3M, FR007互换1Y, FR007互换5Y, SHIBOR互换1Y,
+    % SHIBOR 3M
+    [yield,yield_codes,~,yield_times,yield_error,~] = w.edb(['S0059744,','M1004899,','M1004263,',...
+                                                'M0048483,','M0048486,','M0048490,',...
+                                                'M0048499,','M0017142,'],...
+                                                start_dt,end_dt); 
+    % FR007互换3M, FR007互换1Y, FR007互换5Y, SHIBOR互换1Y,SHIBOR 3M                                        
+    [irs,irs_codes,~,irs_times,irs_error,~] = w.edb('M1004123,M1004126,M1004130,M1004084,M1001858',...
+                                                start_dt,end_dt); 
                                             
+    shi3m_irs3m = yield(:,8) - yield(:,4);
+    cd3m_irs3m = yield(:,2) - yield(:,4);
     
-    [times,ia,ib] = intersect(yield_times,irs_times);
+    shi3ms1y_irs1y = yield(:,7) - yield(:,5);
     
-    irs = irs(ia,:);
-    yield = yield(ib,:);
+    bond1y_irs1y = yield(:,1) - yield(:,5);
+    cdb1y_irs1y = yield(:,3) - yield(:,5);
     
-    shibor3m_irs3m = irs(:,5) - irs(:,1);
-    cd3m_irs3m = yield(:,2) - irs(:,1);
-    
-    shi3ms1y_irs1y = irs(:,4) - irs(:,2);
-    
-    bond1y_irs1y = yield(:,1) - irs(:,1);
-    cdb1y_irs1y = yield(:,3) - irs(:,1);
+    irs1y_irs3m = yield(:,5) - yield(:,4);
+    irs5y_irs1y = yield(:,6) - yield(:,5);
     
 
 end
