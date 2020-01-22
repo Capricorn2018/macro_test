1a79dfa9c38cf84dfae664203d168282efad9b09
Capricorn2018 <zhouxinyu882003@163.com>
2020-01-22 13:57:41 +0800

    plot figures in macro_factor/indicators/liq_prem.m

--- macro_factor/indicators/liq_prem.m
+++ macro_factor/indicators/liq_prem.m
@@ -7,23 +7,77 @@ function res = liq_prem(start_dt,end_dt)
     % 
     [yield,~,~,times,~,~] = w.edb(['M1010882,S0059744,M1004263,M1010885,',...
                                     'M1004123,M1004126,M1004130,M1004084,M1001858'],...
-                                    start_dt,end_dt); %#ok<ASGLU>
+                                    start_dt,end_dt);
                                             
     shi3m_irs3m = yield(:,9) - yield(:,5); % 3mÁ÷¶¯ÐÔÒç¼Û
-    cd3m_irs3m = yield(:,1) - yield(:,5);
+    cd3m_irs3m = yield(:,1) - yield(:,5); % 3m´æµ¥¼õ»¥»»
     
-    shi3ms1y_irs1y = yield(:,8) - yield(:,6); % 1yÁ÷¶¯ÐÔÒç¼Û    
-    bond1y_irs1y = yield(:,2) - yield(:,6);
-    cdb1y_irs1y = yield(:,3) - yield(:,6);
-    cd1y_irs1y = yield(:,4) - yield(:,6);
+    shi3ms1y_irs1y = yield(:,8) - yield(:,6); % 1Y SHIBOR»¥»»¼õ»¥»»  
+    bond1y_irs1y = yield(:,2) - yield(:,6); % 1Y¹úÕ®¼õ»¥»»
+    cdb1y_irs1y = yield(:,3) - yield(:,6); % 1Y¹ú¿ª¼õ»¥»»
+    cd1y_irs1y = yield(:,4) - yield(:,6); % 1Y´æµ¥¼õ»¥»»
+    
+    figure(2);
+    subplot(2,2,1);
+    plot_hist(shi3m_irs3m);
+    title('SHIBOR3M minus IRS3M histogram');
+    subplot(2,2,2);
+    plot(times,shi3m_irs3m);
+    datetick('x','yyyy','keeplimits');
+    title('SHIBOR3M minus IRS3M');
+    axis tight;
+    subplot(2,2,3);
+    plot_hist(cd3m_irs3m);
+    title('CD3M minus IRS3M histogram');
+    subplot(2,2,4);
+    plot(times,cd3m_irs3m);
+    datetick('x','yyyy','keeplimits');
+    title('CD3M minus IRS3M');
+    axis tight;
+    
+    figure(3);
+    subplot(2,2,1);
+    plot_hist(shi3ms1y_irs1y);
+    title('SHIBOR3M1Y minus IRS1Y histogram');
+    subplot(2,2,2);
+    plot_hist(bond1y_irs1y);
+    title('BOND1Y minus IRS1Y histogram');
+    subplot(2,2,3);
+    plot_hist(cdb1y_irs1y);
+    title('CDB1Y minus IRS1Y histogram');
+    subplot(2,2,4);
+    plot_hist(cd1y_irs1y);
+    title('CD1Y minus IRS1Y histogram');
+    
+    figure(4);
+    subplot(2,2,1);
+    plot(times,shi3ms1y_irs1y);
+    datetick('x','yyyy','keeplimits');
+    title('SHIBOR3M1Y minus IRS1Y');
+    axis tight;
+    subplot(2,2,2);
+    plot(times,bond1y_irs1y);
+    datetick('x','yyyy','keeplimits');
+    title('BOND1Y minus IRS1Y');
+    axis tight;
+    subplot(2,2,3);
+    plot(times,cdb1y_irs1y);
+    datetick('x','yyyy','keeplimits');
+    title('CDB1Y minus IRS1Y');
+    axis tight;
+    subplot(2,2,4);
+    plot(times,cd1y_irs1y);
+    datetick('x','yyyy','keeplimits');
+    title('CD1Y minus IRS1Y');
+    axis tight;
+    
     
     res = table(shi3m_irs3m,cd3m_irs3m,shi3ms1y_irs1y,...
                 bond1y_irs1y,cdb1y_irs1y,cd1y_irs1y,...
-                irs1y_irs3m,irs5y_irs1y,...
                 'VariableNames',...
                 {'shi3m_irs3m','cd3m_irs3m','shi3ms1y_irs1y',...
                 'bond1y_irs1y','cdb1y_irs1y','cd1y_irs1y'});
     
-
+    w.close;
 end
 
---
+++ macro_factor/indicators/utilities/str2dates.m
@@ -0,0 +1,7 @@
+function [ output_args ] = str2dates( input_args )
+%STR2DATES 此处显示有关此函数的摘要
+%   此处显示详细说明
+
+
+end
+
