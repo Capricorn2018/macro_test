daf2d397102bf625aa8fe5a8048ba815ba35ebb9
unknown <zhouxy@cmfdev.cmfchina.com>
2020-01-25 21:18:24 +0800

    a new function file cdb_sprd.m added

---
+++ macro_factor/indicators/cdb_sprd.m
@@ -0,0 +1,31 @@
+function [sprd21,sprd31,sprd32,times] =  cdb_sprd(bond_list,start_dt,end_dt)
+% cdb_sprd用来算国开10年新老券利差
+    w = windmatlab;
+
+    
+    [mat,~,~,mat_times,~,~] = w.wsd(bond_list,'matu_cnbd',start_dt,end_dt,'credibility=1');
+    
+    [yield,~,~,yield_times,~,~] = w.wsd(bond_list,'yield_cnbd',start_dt,end_dt,'credibility=1');
+    
+    [times,ia,ib] = intersect(mat_times,yield_times);
+    mat = mat(ia,:);
+    yield = yield(ib,:);
+    
+    sprd31 = nan(size(yield,1),1);
+    sprd21 = nan(size(yield,1),1);
+    sprd32 = nan(size(yield,1),1);
+    
+    for i=1:size(mat,1)
+        mati = mat(i,:);
+        [~,I] = maxk(mati,3);
+        yieldi = yield(i,I);
+        sprd31(i) = yieldi(3) - yieldi(1);
+        sprd21(i) = yieldi(2) - yieldi(1);
+        sprd32(i) = yieldi(3) - yieldi(2);
+        
+    end
+    
+    w.close;
+    
+end
+
