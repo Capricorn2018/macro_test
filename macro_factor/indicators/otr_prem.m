7cd55e5cdde95f17983ae8f327c6c702f528a104
unknown <zhouxy@cmfdev.cmfchina.com>
2020-01-26 20:41:00 +0800

    some changes in otr_prem.m

--- macro_factor/indicators/otr_prem.m
+++ macro_factor/indicators/otr_prem.m
@@ -1,7 +1,10 @@
-function [sprd21,sprd31,sprd32,times] =  otr_prem(bond_list,start_dt,end_dt)
-% otm_prem用来算国开10年新老券利差
+function [sprd21,sprd31,sprd32,times] =  otr_prem(start_dt,end_dt)
+% otm_preme用来算国开10年新老券利差
     w = windmatlab;
-
+    
+    % 从otr_prem.xlsx中读入券表，发新券需要手动更新文件
+    [~,bond_list,~] = xlsread('inputs/otr_prem.xlsx','bond_list');
+    bond_list = strjoin(bond_list,',');
     
     [mat,~,~,mat_times,~,~] = w.wsd(bond_list,'matu_cnbd',start_dt,end_dt,'credibility=1');
     
@@ -24,6 +27,30 @@ function [sprd21,sprd31,sprd32,times] =  otr_prem(bond_list,start_dt,end_dt)
         sprd32(i) = yieldi(3) - yieldi(2);
         
     end
+    
+    subplot(3,2,1);
+    plot_hist(sprd21);
+    title('国开最新券次新券利差分布图');
+    subplot(3,2,2);
+    plot(times,sprd21);    
+    datetick('x','yyyymm','keeplimits');
+    title('国开最新券次新券利差');
+    subplot(3,2,3);
+    plot_hist(sprd31);
+    title('国开最新券次次新券利差分布图');
+    subplot(3,2,4);
+    plot(times,sprd31);
+    datetick('x','yyyymm','keeplimits');
+    title('国开最新券次次新券利差');
+    subplot(3,2,5);
+    plot_hist(sprd32);
+    title('国开次新券次次新券利差分布');
+    subplot(3,2,6);
+    plot(times,sprd32);
+    datetick('x','yyyymm','keeplimits');
+    title('国开次新券次次新券利差');
+    
+    
     
     w.close;
     
