5f00e46dd9cd1dbe377f20b5313a18fe465acae1
Capricorn2018 <zhouxinyu882003@163.com>
2020-02-05 10:00:39 +0800

    completing basis_prem2.m

--- macro_factor/indicators/basis_prem2.m
+++ macro_factor/indicators/basis_prem2.m
@@ -26,19 +26,51 @@ function [res,times,cont_list] = basis_prem2(start_dt,end_dt)
     % 再然后是对应每个券和每个期货合约的转换因子(需要一个一个合约循环)
     % 最后按日循环, 每日先看当天的期货合约是哪三个, 然后读取转换因子、当日净价、和期货收盘价
     % 根据述三种数据算出当日对应的三个期货合约基差最小的券的基差, 作为当日基差
-    [bond,~,~,bond_times,~,~] = w.wsd(bond_list,'net_cnbd',start_dt,end_dt,'credibility=1');
-    [T,~,~,T_times,~,~] = w.wsd(cont_list,'close',start_dt,end_dt);
+    bond = nan(length(ctd_times),length(bond_list));
+    T = nan(length(ctd_times),length(cont_list));
     
-        
+    [bond_wind,~,~,bond_times,~,~] = w.wsd(bond_list,'net_cnbd',start_dt,end_dt,'credibility=1');
+    [T_wind,~,~,T_times,~,~] = w.wsd(cont_list,'close',start_dt,end_dt);
+    
+    % 先做时间对齐避免数据不统一
+    [Lia,Locb] = ismember(bond_times,ctd_times);
+    bond(Locb(~isnan(Locb)),:) = bond_wind(Lia,:);
+    
+    % 先做时间对齐避免数据不统一
+    [Lia,Locb] = ismember(T_times,ctd_times);
+    T(Locb(~isnan(Locb)),:) = T_wind(Lia,:);
+    
+    cf = nan(length(bond_list),length(cont_list));    
     for i=1:length(cont_list)
-        [cf,~,~,cf_times,~,~]=w.wset('conversionfactor',['windcode=',cont_list{i}]);
+        [cf_i,~,~,~,~,~] = w.wset('conversionfactor',['windcode=',cont_list{i}]);
         % 这里还需要注意跟原先的bond_list取交集之后做成一个matrix
+        [Lia,Locb] = ismember(cf_i(:,1),bond_list);
+        cf(Locb(~isnan(Locb)),i) = cf_i(Lia,:);
     end
     
+    
+    res = nan(length(ctd_times),length(cont_list));
     % 按日循环
     for i = 1:length(ctd_times)
         % 这里先用active_cont来取每日活跃合约
+        % 每日T合约编号, 当季:1, 次季:2, 远季:3
+        curr_dt = ctd_times(i);
+        [rk,~] = active_cont(curr_dt,frst_dt,last_dt);
+        
+        
         % 然后利用上面的cf和T以及bond来算当日的basis并且取最小的
+        cf1 = cf(:,rk==1);
+        basis1 = bond(i,:) - cf1 .* T(i,rk==1);
+        res(i,rk==1) = min(basis1);
+        
+        cf2 = cf(:,rk==2);
+        basis2 = bond(i,:) - cf2 .* T(i,rk==2);
+        res(i,rk==2) = min(basis2);        
+        
+        cf(:,rk==3);
+        basis3 = bond(i,:) - cf3 .* T(i,rk==3);
+        res(i,rk==3) = min(basis3);        
+        
     end
     
     w.close;
