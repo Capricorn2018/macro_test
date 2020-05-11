83e28e07d9c53b7176f5948b317ae3257b744570
Capricorn2018 <zhouxinyu882003@163.com>
2020-05-11 14:16:11 +0800

    some changes in test.m

--- cmb/test.m
+++ cmb/test.m
@@ -1,14 +1,17 @@
 w = windmatlab;
 
-start_dt = '2005-01-01';
+start_dt = '2006-06-01';
 end_dt = '2020-04-24';
 
-% 国开债1,3,5,7,10; 国债5y                                            
-[edb_data,edb_codes,~,edb_times,~,~] = w.edb('M1004263,M1004265,M1004267,M1004269,M1004271,S0059747',start_dt,end_dt,'Fill=Previous'); 
+% 国开债1,3,5,7,10;
+% 国债1,3,5,7,10
+[edb_data,edb_codes,~,edb_times,~,~] = w.edb(['M1004263,M1004265,M1004267,M1004269,M1004271,',...
+                                                'M1000158,M1000160,M1000162,M1000164,M1000166'],...
+                                                start_dt,end_dt,'Fill=Previous'); 
 edb = array2table(edb_data,'VariableNames',edb_codes);
 
-% 中债国债即期收益率1,3,5,7,10y
-[edb_data2,edb_codes2,~,edb_times2,~,~] = w.edb('M1001102,M1001104,M1001106,M1001108,M1001110',start_dt,end_dt,'Fill=Previous'); 
+% 国开债即期收益率1,3,5,7,10y
+[edb_data2,edb_codes2,~,edb_times2,~,~] = w.edb('M1004281,M1004283,M1004285,M1004287,M1004289',start_dt,end_dt,'Fill=Previous'); 
 edb2 = array2table(edb_data2,'VariableNames',edb_codes2);
 mat = [1;3;5;7;10];
 fwd = nan(size(edb_data2));
@@ -30,7 +33,12 @@ end
  factors.sprd105 = edb.M1004271 - edb.M1004267;
 
  % 流动性溢价因子, 用国开5-国债5
- factors.liq5y = edb.M1004267 - edb.S0059747;
+ %factors.liq5y = edb.M1004267 - edb.M1000162;
+ 
+ % 国债期限利差因子
+ %  factors.bond51 = edb.M1000162 - edb.M1000158;
+ %  factors.bond105 = edb.M1000166 - edb.M1000162;
+ %factors.curv = edb.M1000162 * 2 - edb.M1000158 - edb.M1000162;
 
 
  reb = find_month_dates(5, ret_times, 'last'); 
@@ -49,14 +57,14 @@ end
  f = table2array(f);
  f = [ones(size(f,1),1),f];
  
-%  [~,Locb] = ismember(reb,edb_times2);
-%  fwd = fwd(Locb,:);
+ [~,Locb] = ismember(reb,edb_times2);
+ fwd = fwd(Locb,:);
  
  y = y(1:end-1);
-%  fwd = fwd(1:end-1,:);
+ fwd = fwd(1:end-1,:);
  f = f(1:end-1,:);
 
- window = 72;
+ window = 40;
  
  pred = nan(length(y),1);
  intercept = pred;
