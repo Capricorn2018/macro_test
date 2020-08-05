0726863e68ad7bd13071f37c9467be127bea3f98
unknown <zhouxy@cmfdev.cmfchina.com>
2020-08-05 16:57:00 +0800

    a plot function added

---
+++ macro_factor/plot_acc_rtn.m
@@ -0,0 +1,36 @@
+function   plot_acc_rtn(s1,s2,v)
+
+    % s1 s2  = [DATENUM, CUM_NAV]
+    % s2 为基准
+
+    [~,ia,ib] = intersect(s1(:,1),s2(:,1));
+    s = [s1(ia,:),s2(ib,2)];
+    s(:,2) = s(:,2)/s(1,2);
+    s(:,3) = s(:,3)/s(1,3);
+
+    subplot(4,1,[1,2])
+    plot(s(:,1),s(:,2),'r','LineWidth',2); hold on;
+    plot(s(:,1),s(:,3),'k','LineWidth',1); hold on;
+    datetick('x','yy','keeplimits')
+    legend(v{1},v{2},'Location','northwest')
+    legend('boxoff')
+    axis tight
+
+    subplot(4,1,3)
+    z = get_cum_exces_nav(s(:,2),s(:,3));
+    plot(s(:,1),z,'k','LineWidth',1); hold on;
+    datetick('x','yy','keeplimits')
+    legend('累计超额收益','Location','northwest')
+    legend('boxoff')
+    axis tight
+
+    subplot(4,1,4)
+    u = get_dd_nav(z);
+    area(s(:,1),u); hold on;
+    datetick('x','yy','keeplimits')
+    legend('超额收益回撤','Location','south')
+    legend('boxoff')
+    grid on
+    axis tight
+
+end
