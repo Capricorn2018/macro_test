78426641ceee140b16ab153e87b600274e80f9e1
unknown <zhouxy@cmfdev.cmfchina.com>
2020-08-11 10:02:39 +0800

    basis_analysis modified

--- macro_factor/indicators/basis_analysis.m
+++ macro_factor/indicators/basis_analysis.m
@@ -1,9 +1,9 @@
-start_dt = '20130901';
-end_dt = '20200806';
+start_dt = '20150101';
+end_dt = '20200807';
 
-% [basis,calender,dominant,dominant_basis,times,cont_list] = basis_prem(start_dt,end_dt);
+%[basis,calender,dominant,dominant_basis,times,cont_list] = basis_prem(start_dt,end_dt);
 
-% [curve,yield] = term_prem(start_dt,end_dt);
+%[curve,yield] = term_prem(start_dt,end_dt);
 
 x1 = curve.bond5y_bond1y;
 x2 = yield.bond1y;
@@ -15,13 +15,17 @@ x1 = x1(ib);
 x2 = x2(ib);
 y = dominant_basis(ia);
 
+y2 = dominant(ia);
+
 mdl = fitlm([x1,x2],y);
 
 y_p = mdl.Fitted;
 
+figure(5);
 plot(C,y);
 hold on;
 plot(C,y_p);
+plot(C,y2);
 datetick('x','yyyymm','keeplimits');
-legend('basis','pred_basis');
+legend('basis','pred_basis','calender');
 hold off;
